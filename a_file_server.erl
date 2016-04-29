b484709e20ffd39634d03a354475168b323f5612
Paul Myjavec <pauly@buzbox.net>
2016-04-29 16:01:22 +0200

    initial

---
+++ a_file_server.erl
@@ -0,0 +1,12 @@
+-module(a_file_server).
+-export([start/1, loop/1]).
+start(Dir) -> spawn(a_file_server, loop, [Dir]).
+loop(Dir) ->
+        receive
+        {Client, list_dir} ->
+            Client ! {self(), file:list_dir(Dir)};
+        {Client, {get_file, File}} ->
+                        Full = filename:join(Dir, File),
+                        Client ! {self(), file:read_file(Full)}
+        end,
+        loop(Dir).
