f1b5714a97c9eb11f3f2b3fe92cca52ca05f39d3
Paul Myjavec <pauly@buzbox.net>
2016-05-03 16:58:22 +0200

    Added put_file capability to client and server

--- a_client.erl
+++ a_client.erl
@@ -1,5 +1,5 @@
 -module(a_client).
--export([ls/1,get_file/2]).
+-export([ls/1,get_file/2,put_file/3]).
 ls(Server) ->
         Server ! {self(), list_dir},
         receive
@@ -9,6 +9,12 @@ ls(Server) ->
 get_file(Server,File) ->
         Server ! {self(), {get_file, File}},
         receive
-                {Server,Content} ->
+                {Server, Content} ->
                         Content
+        end.
+put_file(Server,File,Content) ->
+        Server ! {self(), {put_file, File, Content}},
+        receive
+                {Server, Response} ->
+                        Response
         end.
--- a_file_server.erl
+++ a_file_server.erl
@@ -3,9 +3,12 @@
 start(Dir) -> spawn(a_file_server, loop, [Dir]).
 loop(Dir) ->
         receive
-        {Client, list_dir} ->
-            Client ! {self(), file:list_dir(Dir)};
-        {Client, {get_file, File}} ->
+                {Client, list_dir} ->
+                        Client ! {self(), file:list_dir(Dir)};
+                {Client, {put_file, File, Contents}} ->
+                        Full = filename:join(Dir, File),
+                        Client ! {self(), file:write_file(Full, Contents)};
+                {Client, {get_file, File}} ->
                         Full = filename:join(Dir, File),
                         Client ! {self(), file:read_file(Full)}
         end,
