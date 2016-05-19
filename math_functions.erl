9bb42323d4c013531c8e2c7c8a907e19926d1a28
Paul Myjavec <pauly@buzbox.net>
2016-05-19 17:24:26 +0200

    Math library tidy up

--- math_functions.erl
+++ math_functions.erl
@@ -1,6 +1,7 @@
 -module(math_functions).
 -export([even/1,odd/1, test/0, filter/2, odds_and_evens1/1, odds_and_evens2/1]).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TESTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test() ->
     true = even(40),
     false = even(39),
@@ -8,22 +9,21 @@ test() ->
     false = odd(20),
     [2,4] = test_filter(),
     {[2,4], [1,3]} = odds_and_evens1([1,2,3,4]),
-    {[20,30], [11,3]} = odds_and_evens2([11,20,3,30]),
+    {[30,20], [3,11]} = odds_and_evens2([11,20,3,30]),
     tests_passed.
 
 test_filter() ->
     F = fun (X) -> even(X) end,
     filter(F,[1,2,3,4]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 even(I) when (I rem 2) == 0 ->
     true;
 even(I) when (I rem 2) /= 0 ->
     false.
 
-odd(I) when (I rem 2) /= 0 ->
-    true;
-odd(I) when (I rem 2) == 0 ->
-    false.
+odd(I) ->
+    not(even(I)).
 
 filter(F,L) -> [ X || X <- L, F(X) == true ].
 
@@ -43,4 +43,4 @@ quick_split([H|T], Odds, Evens) ->
         0 -> quick_split(T, Odds, [H|Evens])
     end;
 quick_split([], Odds, Evens) ->
-    {Odds, Evens}.
+    {Evens, Odds}.
